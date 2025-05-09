914fcf6ba0cfc93e49c4b7a93607e09a39789efd
shirinovashukriyye <shirinovashukriyye@gmail.com>
2025-05-09 21:11:18 +0400

    project

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,43 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let users = JSON.parse(localStorage.getItem("users")) || [];
+  let users = JSON.parse(localStorage.getItem("users"));
+  let form = document.querySelector(".form");
+  let username = document.querySelector("#username");
+  let password = document.querySelector("#password");
 
-    let form = document.querySelector("form");
-    let username = document.querySelector("#username");
-    let password = document.querySelector("#password");
-
-    form.addEventListener("submit", login);
-
-    function login(e) {
-        e.preventDefault();
-
-        let loginedUser = users.find(user =>
-            user.username === username.value && user.password === password.value
-        );
-
-        if (loginedUser) {
-            loginedUser.isLogged = true;
-            localStorage.setItem("users", JSON.stringify(users));
-            toatifyByPage("Giriş uğurludur!");
-
-            setTimeout(() => {
-                window.location.href = "index.html";
-            }, 2000);
-        } else {
-            toatifyByPage("İstifadəçi adı və ya şifrə yalnışdır!");
-        }
+  function login(e) {
+    e.preventDefault();
+    if (!users) {
+      toast("No users found");
+      return;
     }
 
-    let toatifyByPage = (text) => {
-        Toastify({
-            text: `${text}`,
-            duration: 3000,
-            close: true,
-            gravity: "top",
-            position: "left",
-            stopOnFocus: true,
-            style: {
-                background: "linear-gradient(to right, #00b09b, #96c93d)",
-            }
-        }).showToast();
-    };
+    let findUser = users.find(
+      (user) =>
+        user.username === username.value && user.password === password.value
+    );
+
+    if (findUser) {
+      findUser.isLogined = true;
+      localStorage.setItem("users", JSON.stringify(users));
+      toast("User logged in successfully");
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 2000);
+    } else {
+      toast("User or password is incorrect");
+      return;
+    }
+  }
+
+  function toast(text) {
+    Toastify({
+      text: `${text}`,
+      duration: 2000,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
+  }
+
+  form.addEventListener("submit", login);
 });
--- assets/js/register.js
+++ assets/js/register.js
@@ -1,94 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let users = JSON.parse(localStorage.getItem("users")) || [];
+  let users = JSON.parse(localStorage.getItem("users")) || [];
 
-    let form = document.querySelector("#register-form");
-    let name = document.querySelector("#name");
-    let username = document.querySelector("#username");
-    let email = document.querySelector("#email");
-    let password = document.querySelector("#password");
-    let confirmPassword = document.querySelector("#confirmpassword");
-    let truePassword=document.querySelector(".fa-check")
-    let falsePasswordStyle=document.querySelector(".fa-x")
-    form.addEventListener("submit", register);
+  let form = document.querySelector(".form");
+  let name = document.querySelector("#name");
+  let surname = document.querySelector("#surname");
+  let username = document.querySelector("#username");
+  let email = document.querySelector("#email");
+  let password = document.querySelector("#password");
+  let isLogined = false;
 
-    function register(e) {
-        e.preventDefault();
-        let uniqueUser = users.some(
-            (user) => user.username === username.value || user.email === email.value
-        );
-        let id = uuid.v4();
+  function register(e) {
+    e.preventDefault();
+    let id = uuidv4();
+    let uniqueUser = users.some(
+      (user) => user.username === username.value || user.email === email.value
+    );
 
-        if (username.value.length < 3 || username.value.length > 20) {
-            toatifyByPage("Username must be between 3 and 20 characters");
-            return;
-        }
-
-        if (password.value !== confirmPassword.value) {
-            toatifyByPage("Passwords do not match");
-            return;
-        }
-
-        if (password.value.length<=8) {
-            truePassword.classList.add("d-none");
-            truePassword.classList.remove("d-block");
-            falsePasswordStyle.classList.add("d-block");
-            falsePasswordStyle.classList.remove("d-none");
-            toatifyByPage("Password is not long enough (min character 8!");
-            return;
-        }
-        else{
-            falsePasswordStyle.classList.add("d-none"); 
-            falsePasswordStyle.classList.remove("d-block");
-            truePassword.classList.add("d-block"); 
-            truePassword.classList.remove("d-none");
-        }
-
-        let usernameExists = users.some(user => user.username === username.value.trim());
-        let emailExists = users.some(user => user.email === email.value.trim());
-
-        if (usernameExists) {
-            toatifyByPage("Username already exists");
-            return;
-        }
-
-        if (emailExists) {
-            toatifyByPage("Email has been used");
-            return;
-        }
-
-        if (!uniqueUser) {
-            let newUser = {
-                name: name.value,
-                username: username.value,
-                email: email.value,
-                password: password.value,
-                confirmPassword: confirmPassword.value,
-                id,
-                isLogged: false,
-                wishlist:[],
-                basket:[]
-            };
-
-            users.push(newUser);
-            localStorage.setItem("users", JSON.stringify(users));
-            form.reset();
-            toatifyByPage("User registered successfully");
-        } else {
-            alert("This user already exists!");
-        }
+    if (!uniqueUser) {
+      let newUser = {
+        name: name.value,
+        surname: surname.value,
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        isLogined: isLogined,
+        id: id,
+        wishList: [],
+        basket: [],
+        role: users.length === 0 ? "admin" : "user", 
+      };
+      users.push(newUser);
+      localStorage.setItem("users", JSON.stringify(users));
+      toast("User registered successfully");
+      setTimeout(() => {
+        window.location.href = "login.html";
+      }, 2000);
+    } else {
+      toast("User already exists");
+      return;
     }
+  }
 
-});
-
-let toatifyByPage = (text) => {
+  function toast(text) {
     Toastify({
-        text: `${text}`,
-        duration: 3000,
-        gravity: "top",
-        position: "left",
-        stopOnFocus: true,
-        style: {
-            background: "linear-gradient(to right, #00b09b, #96c93d)",
-        },
+      text: `${text}`,
+      duration: 2000,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
     }).showToast();
-};
+  }
+
+  form.addEventListener("submit", register);
+});
